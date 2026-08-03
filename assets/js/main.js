7ff743fe15867acc0774240b20b206b4b580d911
rez1k <bahtinovdm@gmail.com>
2026-08-03 11:48:34 +0400

    feat: publish textileopttorg branding

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-import { initLeadForm } from './form.js?v=modal-lock-20260802-3';
-import { initMenu } from './menu.js?v=modal-lock-20260802-3';
-import { initCookieBanner } from './cookies.js?v=modal-lock-20260802-3';
-import { initVideoDialog } from './video.js?v=modal-lock-20260802-3';
+import { initLeadForm } from './form.js?v=brand-logo-20260803-1';
+import { initMenu } from './menu.js?v=brand-logo-20260803-1';
+import { initCookieBanner } from './cookies.js?v=brand-logo-20260803-1';
+import { initVideoDialog } from './video.js?v=brand-logo-20260803-1';
 
 initLeadForm(document.querySelector('#consultation-form'), {
   previewMode: document.documentElement.dataset.staticPreview === 'true'
