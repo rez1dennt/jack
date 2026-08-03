8aabd99e09e791883873ff0b7994c19521a5c345
rez1k <bahtinovdm@gmail.com>
2026-08-03 14:28:21 +0400

    feat: publish organization details preview

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-import { initLeadForm } from './form.js?v=logo-alpha-20260803-1';
-import { initMenu } from './menu.js?v=logo-alpha-20260803-1';
-import { initCookieBanner } from './cookies.js?v=logo-alpha-20260803-1';
-import { initVideoDialog } from './video.js?v=logo-alpha-20260803-1';
+import { initLeadForm } from './form.js?v=organization-details-20260803-1';
+import { initMenu } from './menu.js?v=organization-details-20260803-1';
+import { initCookieBanner } from './cookies.js?v=organization-details-20260803-1';
+import { initVideoDialog } from './video.js?v=organization-details-20260803-1';
 
 initLeadForm(document.querySelector('#consultation-form'), {
   previewMode: document.documentElement.dataset.staticPreview === 'true'
