e7c54089285daa86b1925636b2ab1c772259e76b
rez1k <bahtinovdm@gmail.com>
2026-08-06 17:26:00 +0400

    build: publish short economics section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-import { initLeadForm } from './form.js?v=model-speed-sync-20260805-1';
-import { initMenu } from './menu.js?v=model-speed-sync-20260805-1';
-import { initCookieBanner } from './cookies.js?v=model-speed-sync-20260805-1';
-import { initVideoDialog } from './video.js?v=model-speed-sync-20260805-1';
-import { initSpecTabs } from './spec-tabs.js?v=model-speed-sync-20260805-1';
+import { initLeadForm } from './form.js?v=economics-short-20260806-1';
+import { initMenu } from './menu.js?v=economics-short-20260806-1';
+import { initCookieBanner } from './cookies.js?v=economics-short-20260806-1';
+import { initVideoDialog } from './video.js?v=economics-short-20260806-1';
+import { initSpecTabs } from './spec-tabs.js?v=economics-short-20260806-1';
 
 initLeadForm(document.querySelector('#consultation-form'), {
   previewMode: document.documentElement.dataset.staticPreview === 'true'
