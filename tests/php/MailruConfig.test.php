5c0324da21a491981f109c1802da26e693b7f530
rez1k <bahtinovdm@gmail.com>
2026-08-07 15:29:59 +0400

    docs: document Mail.ru SMTP deployment

--- tests/php/MailruConfig.test.php
+++ tests/php/MailruConfig.test.php
@@ -11,3 +11,13 @@ assert($smtp['password'] === '[[MAILRU_APP_PASSWORD]]');
 assert($smtp['encryption'] === 'ssl');
 assert($smtp['from_email'] === 'tekstilopttorg@mail.ru');
 assert($smtp['to_email'] === 'tekstilopttorg@mail.ru');
+
+$readme = file_get_contents(dirname(__DIR__, 2) . '/README.md');
+assert(is_string($readme));
+assert(str_contains($readme, 'smtp.mail.ru'));
+assert(str_contains($readme, '465'));
+assert(str_contains($readme, 'SSL/TLS'));
+assert(str_contains($readme, '[[MAILRU_APP_PASSWORD]]'));
+assert(!str_contains($readme, 'smtp.gmail.com'));
+assert(!str_contains($readme, '[[GOOGLE_APP_PASSWORD]]'));
+assert(!str_contains($readme, 'Настройка Google SMTP'));
